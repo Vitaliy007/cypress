7d508d012b4a458ee4f091c2f57c59f9e2d70fca
Vitaliy Falaleyev <vitaliy.lviv88@gmail.com>
2019-11-22 20:18:36 +0200

    before alias commit

--- cypress/integration/examples/ea.spec.js
+++ cypress/integration/examples/ea.spec.js
@@ -4,13 +4,16 @@ describe("Testing ea app", () => {
     it("Login app", () => {
         cy.visit("http://eaapp.somee.com/");
 
-        cy.contains("Login").click();
+        cy.get('#loginLink').then(($link) => {
+            const linkText = $link.text();
+            expect(linkText).is.eql('Login');   
+        }).click();
         cy.url().should('include', "Account/Login");
         cy.get('#UserName').type('admin');
         cy.get('#Password').type('password');
         cy.get('.btn').click();
         cy.contains("Employee List").click();
         cy.get('.table').find('tr').contains('Prashanth').parent().contains('Benefits').click();
-        
+
     })
 })
